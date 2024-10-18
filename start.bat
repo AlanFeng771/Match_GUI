d9b196003b859eb4fe3cb04b42746a693152ba8d
AlanFeng771 <alan1004224@gmail.com>
2024-10-18 15:52:18 +0800

    update basic file

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 python Controller.py ^
---annotation_file E:\workspace\python\Tools\check_nodule_classification\lung_M_class_0001-1800.csv ^
---bbox_annotation_file output\sub_patient4.csv ^
+--annotation_file lung_M_class_0001-1800.csv ^
+--bbox_annotation_file bbox_annotation.csv ^
 --patient_ids_file patient_ids\patient_ids_sub4.txt ^
 --image_root D:\Bme_Dataset\Raw_npy ^
 --output_file_name sub_patient4 ^
